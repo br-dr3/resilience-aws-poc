943824f5a061055ba964ccdcbb1f0bcdf78184f2
Brian Andreossi <brian_aandreossi@hotmail.com>
2024-07-05 11:22:26 -0300

    chore: change script to use message.json

--- run_local/scripts/send-message.sh
+++ run_local/scripts/send-message.sh
@@ -2,4 +2,4 @@
 
 aws --endpoint-url "http://localhost:4566" sqs send-message \
     --queue-url "http://sqs.sa-east-1.localhost.localstack.cloud:4566/000000000000/resilience-queue" \
-    --message-body "$1"
+    --cli-input-json "file://message.json"
