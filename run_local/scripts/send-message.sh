297e8bd0d8f70e53a9672c2772e374958ee348a6
Brian Andreossi <brian_aandreossi@hotmail.com>
2024-07-05 11:18:07 -0300

    feat: added send-message script

--- run_local/scripts/send-message.sh
+++ run_local/scripts/send-message.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws --endpoint "http://localhost:4566" sqs send-message \
-    --queue-url "http://localhost:4566/000000000000/resilience-queue" \
-    --message-body "Oi"
+aws --endpoint-url "http://localhost:4566" sqs send-message \
+    --queue-url "http://sqs.sa-east-1.localhost.localstack.cloud:4566/000000000000/resilience-queue" \
+    --message-body "$1"
