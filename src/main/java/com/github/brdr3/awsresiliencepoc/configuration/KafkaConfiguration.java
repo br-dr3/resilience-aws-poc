7700d31a489cfede025e442a5997b38f56b855cb
Brian Andreossi <brian_aandreossi@hotmail.com>
2024-07-05 15:28:48 -0300

    feat: add kafka produce

---
+++ src/main/java/com/github/brdr3/awsresiliencepoc/configuration/KafkaConfiguration.java
@@ -0,0 +1,56 @@
+package com.github.brdr3.awsresiliencepoc.configuration;
+
+import lombok.Getter;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.Map;
+
+@Getter
+@EnableAsync
+@Configuration
+public class KafkaConfiguration {
+
+    private static final short REPLICATION_FACTOR = 1;
+
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${kafka.topic-name}")
+    private String topicName;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers));
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return new NewTopic(topicName, 1, REPLICATION_FACTOR);
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        final Map<String, Object> properties = Map.ofEntries(
+                Map.entry(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers),
+                Map.entry(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class),
+                Map.entry(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class));
+
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(final ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
--- src/main/java/com/github/brdr3/awsresiliencepoc/listener/ResilienceListener.java
+++ src/main/java/com/github/brdr3/awsresiliencepoc/listener/ResilienceListener.java
@@ -1,15 +1,21 @@
 package com.github.brdr3.awsresiliencepoc.listener;
 
+import com.github.brdr3.awsresiliencepoc.service.KafkaService;
 import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
 import io.awspring.cloud.messaging.listener.annotation.SqsListener;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ResilienceListener {
+
+    private final KafkaService kafkaService;
 
     @SqsListener(value = "${sqs.queue-name}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void handle(Message<String> message) {
-        System.out.println(message.getPayload());
+        System.out.println("Mensagem recebida..");
+        kafkaService.sendMessage(message.getPayload());
     }
 }
