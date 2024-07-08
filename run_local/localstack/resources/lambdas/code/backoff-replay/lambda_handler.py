b68f3766a22637ff8f992fe6feba81f57b64b5c0
Brian Andreossi <brian_aandreossi@hotmail.com>
2024-07-07 23:21:30 -0300

    feat: simple lambda sending message to replay queue

--- run_local/localstack/resources/lambdas/code/backoff-replay/lambda_handler.py
+++ run_local/localstack/resources/lambdas/code/backoff-replay/lambda_handler.py
@@ -1,2 +1,22 @@
+import boto3
+import os
+
+sqs = boto3.client("sqs")
+queue_destination = os.environ.get("QUEUE_DESTINATION")
+
 def handler(event, context):
-    print(event)
+
+    print(f"Handling {len(event)} messages.")
+
+    messages = event["Records"]
+    for message in messages:
+        handler_message(message)
+
+def handler_message(message):
+    sqs.send_message(
+        QueueUrl=queue_destination,
+        MessageBody=message["body"],
+        MessageAttributes=message["messageAttributes"]
+    )
+
+    print("Message redirected with success")
